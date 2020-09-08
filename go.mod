a76b8570769c5d7206b8aec094c8455f68c07954
Luis Schweigard <luis.schweigard@hs-augsburg.de>
2020-09-08 20:35:21 +0200

    Implemented minimal go application using RabbitMQ

---
+++ go.mod
@@ -0,0 +1,5 @@
+module go-distributed
+
+go 1.14
+
+require github.com/streadway/amqp v1.0.0 // indirect
---
+++ src/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"github.com/streadway/amqp"
+	"log"
+)
+
+func main() {
+	go client()
+	go server()
+
+	// Keep main thread alive
+	var s string
+	_, _ = fmt.Scanln(&s)
+}
+
+func client() {
+	conn, ch, q := getQueue()
+	defer conn.Close()
+	defer ch.Close()
+
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnErr(err, "Failed to register a consumer")
+
+	for msg := range msgs {
+		log.Printf("Received message: %s", msg.Body)
+	}
+}
+
+func server() {
+	conn, ch, q := getQueue()
+	defer conn.Close()
+	defer ch.Close()
+
+	msg := amqp.Publishing{
+		ContentType: "text/plain",
+		Body:        []byte("Hello RabbitMQ"),
+	}
+
+	for {
+		_ = ch.Publish("", q.Name, false, false, msg)
+	}
+}
+
+func getQueue() (*amqp.Connection, *amqp.Channel, *amqp.Queue) {
+	conn, err := amqp.Dial("amqp://guest@localhost:5672")
+	failOnErr(err, "Failed to establish connection to RabbitMQ")
+
+	ch, err := conn.Channel()
+	failOnErr(err, "Failed to open a channel")
+
+	q, err := ch.QueueDeclare(
+		"hello",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+	failOnErr(err, "Failed to declare a queue")
+
+	return conn, ch, &q
+}
+
+func failOnErr(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", err, msg)
+	}
+}
